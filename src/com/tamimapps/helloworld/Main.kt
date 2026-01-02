a087ec6b78d76ba4407e6808e7aea17e2556dca3
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-02 19:55:51 +0600

    first commit

---
+++ src/com/tamimapps/helloworld/Main.kt
@@ -0,0 +1,48 @@
+package com.tamimapps.helloworld
+
+import kotlin.reflect.typeOf
+
+
+fun main() {
+    val myUserName: String = "Hasnath Ahmed"
+    val myId: Int = 1010101
+    var price: Double = 100.56
+
+    val myPurchaseArray: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+    val mydoublePurchaseArray: DoubleArray = doubleArrayOf(30.45, 45.30)
+    val allTpeArray: Array<Any> = arrayOf(false, 34.5f, "tamim")
+    val number1: String = "11a"
+    val number2: String = "22"
+
+//    val covertNumberSum: Int = number1.toInt() + number2.toInt()
+//    println(covertNumberSum)
+
+
+    try {
+        val sum = number1.toInt() + number2.toInt()
+        println("Sum = $sum")
+    } catch (e: NumberFormatException) {
+        println("Conversion failed: Invalid number ${e.message}")
+    }
+    val a = 103
+    val b = 1012
+
+    val c: Int = if (a < b) {
+        val sum = a + b
+        sum
+    } else {
+        0   // must return an Int
+    }
+
+    println(c)
+
+
+//    println(a == b)
+
+//    println(allTpeArray.contentToString())
+//    println("my purchase array is ${myPurchaseArray.contentToString()} and /n my double purchase array ${mydoublePurchaseArray.contentToString()}")
+//    println("My username is $myUserName and ID is $myId")
+//    println("My username is $price")
+
+//   println("Hello World! This is my value that is :  ${args[0]} and second is ${args[1]}")
+}
